fccc7d88b087365a4fa13190ed40c2f6afec0b81
leobessa <leobessa@gmail.com>
2023-11-03 16:58:43 -0300

    Update ex_typst_nif.ex

--- lib/ex_typst_nif.ex
+++ lib/ex_typst_nif.ex
@@ -4,27 +4,30 @@ defmodule ExTypst.NIF do
   mix_config = Mix.Project.config()
   version = mix_config[:version]
   github_url = mix_config[:package][:links]["GitHub"]
-
-  targets = ~w(
-    arm-unknown-linux-gnueabihf
-    aarch64-apple-darwin
-    aarch64-unknown-linux-gnu
-    aarch64-unknown-linux-musl
-    riscv64gc-unknown-linux-gnu
-    x86_64-apple-darwin
-    x86_64-pc-windows-gnu
-    x86_64-pc-windows-msvc
-    x86_64-unknown-linux-gnu
-    x86_64-unknown-linux-musl
-  )
+  # Since Rustler 0.27.0, we need to change manually the mode for each env.
+  # We want "debug" in dev and test because it's faster to compile.
+  mode = if Mix.env() in [:dev, :test], do: :debug, else: :release
 
   use RustlerPrecompiled,
     otp_app: :ex_typst,
     crate: "extypst_nif",
-    base_url: "#{github_url}/releases/download/v#{version}",
-    force_build: System.get_env("EX_TYPST_RUSTLER_BUILD") in ["1", "true"],
     version: version,
-    targets: targets
+    base_url: "#{github_url}/releases/download/v#{version}",
+    targets: ~w(
+      aarch64-apple-darwin
+      aarch64-unknown-linux-gnu
+      aarch64-unknown-linux-musl
+      x86_64-apple-darwin
+      x86_64-pc-windows-msvc
+      x86_64-pc-windows-gnu
+      x86_64-unknown-linux-gnu
+      x86_64-unknown-linux-musl
+      x86_64-unknown-freebsd
+    ),
+    # We don't use any features of newer NIF versions, so 2.15 is enough.
+    nif_versions: ["2.15"],
+    mode: mode,
+    force_build: System.get_env("EX_TYPST_RUSTLER_BUILD") in ["1", "true"]
 
   def compile(_content, _font_paths), do: :erlang.nif_error(:nif_not_loaded)
 end
