776606bf5cb60783d2359ccd96e608bde23a4dd3
leobessa <leobessa@gmail.com>
2023-11-06 11:56:11 -0300

    Update checksum-Elixir.ExTypst.NIF.exs

--- checksum-Elixir.ExTypst.NIF.exs
+++ checksum-Elixir.ExTypst.NIF.exs
@@ -1,3 +1,9 @@
 %{
-  "libextypst_nif-v0.2.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:10aea7c097fc8ffa499bc0e7dbcea984eac8b1d716c16803edf7712f46fd2f12",
+  "extypst_nif-v0.2.3-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:5c970f994b4c1cd41f9843dcf4e417fd18d1b35ae98ccc63849c27ab942b3a6b",
+  "libextypst_nif-v0.2.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:73ae7ab81a87370aa4746b89ab114de1e1c32607bc417cc645f60188a1d2e952",
+  "libextypst_nif-v0.2.3-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:37a296e7a8cc63d7cff95810fea8268df747858fcad75ce0e28e6c0b16c94d64",
+  "libextypst_nif-v0.2.3-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:b481bd44d824d2340e62b320857a43f12acbbd591e61ee29f1fee9f1c298c70a",
+  "libextypst_nif-v0.2.3-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:beb543d7d59bb6d58cd01957c859c1b272e498249352ec3b1dd34d2c3402ef10",
+  "libextypst_nif-v0.2.3-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:9df5f5dcc472425dc5d4ab773579f0381d70266db6216c774b77c239072aab0f",
+  "libextypst_nif-v0.2.3-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:60d6d84d71e7c35523d0be4e98d8993e4abee6476a4c45513cadac550db5962e",
 }
