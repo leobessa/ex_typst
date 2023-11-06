8d5adb701a64c5c4ead43c08339c9e908348e8cc
leobessa <leobessa@gmail.com>
2023-11-06 11:17:33 -0300

    fix: remove x86_64-pc-windows-gnu target

--- lib/ex_typst_nif.ex
+++ lib/ex_typst_nif.ex
@@ -19,7 +19,6 @@ defmodule ExTypst.NIF do
       aarch64-unknown-linux-musl
       x86_64-apple-darwin
       x86_64-pc-windows-msvc
-      x86_64-pc-windows-gnu
       x86_64-unknown-linux-gnu
       x86_64-unknown-linux-musl
       x86_64-unknown-freebsd
