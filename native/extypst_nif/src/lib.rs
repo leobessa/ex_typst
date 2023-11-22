ea0f2c58fc79d33f0e351c7bef560c1440efdaf8
Osoro Bironga <fanosoro@gmail.com>
2023-11-22 14:59:21 +0300

    fix: pin typst version

--- native/extypst_nif/src/lib.rs
+++ native/extypst_nif/src/lib.rs
@@ -2,7 +2,7 @@ use std::cell::{RefCell, RefMut};
 use std::collections::HashMap;
 use std::fs::{self, File};
 use std::hash::Hash;
-use std::path::{Path, PathBuf};
+use std::path::{Component, Path, PathBuf};
 
 use comemo::Prehashed;
 use elsa::FrozenVec;
@@ -10,12 +10,13 @@ use memmap2::Mmap;
 use once_cell::unsync::OnceCell;
 use same_file::Handle;
 use siphasher::sip128::{Hasher128, SipHasher13};
-use typst::diag::{FileError, FileResult, StrResult};
-use typst::eval::Library;
-use typst::font::{Font, FontBook, FontInfo};
-use typst::syntax::{Source, SourceId};
-use typst::util::{Buffer, PathExt};
-use typst::World;
+use typst::{
+    diag::{FileError, FileResult, StrResult},
+    eval::{Bytes, Datetime, Library},
+    font::{Font, FontBook, FontInfo},
+    syntax::{FileId, Source, VirtualPath},
+    World,
+};
 use walkdir::WalkDir;
 
 /// A world that provides access to the operating system.
@@ -27,7 +28,7 @@ pub struct SystemWorld {
     hashes: RefCell<HashMap<PathBuf, FileResult<PathHash>>>,
     paths: RefCell<HashMap<PathHash, PathSlot>>,
     sources: FrozenVec<Box<Source>>,
-    main: SourceId,
+    main: Source,
 }
 
 /// Holds details about the location of a font and lazily the font itself.
@@ -41,40 +42,33 @@ struct FontSlot {
 /// Holds canonical data for all paths pointing to the same entity.
 #[derive(Default)]
 struct PathSlot {
-    source: OnceCell<FileResult<SourceId>>,
-    buffer: OnceCell<FileResult<Buffer>>,
+    source: OnceCell<FileResult<FileId>>,
+    buffer: OnceCell<FileResult<Bytes>>,
 }
 
 impl World for SystemWorld {
-    fn root(&self) -> &Path {
-        &self.root
-    }
-
     fn library(&self) -> &Prehashed<Library> {
         &self.library
     }
 
-    fn main(&self) -> &Source {
-        self.source(self.main)
-    }
-
-    fn resolve(&self, path: &Path) -> FileResult<SourceId> {
-        self.slot(path)?
-            .source
-            .get_or_init(|| {
-                let buf = read(path)?;
-                let text = String::from_utf8(buf)?;
-                Ok(self.insert(path, text))
-            })
-            .clone()
-    }
-
-    fn source(&self, id: SourceId) -> &Source {
-        &self.sources[id.into_u16() as usize]
-    }
-
     fn book(&self) -> &Prehashed<FontBook> {
         &self.book
+    }
+
+    fn main(&self) -> Source {
+        self.main.clone()
+    }
+
+    fn source(&self, id: FileId) -> FileResult<Source> {
+        self.sources
+            .iter()
+            .find(|&needle| needle.id().eq(&id))
+            .cloned()
+            .ok_or_else(|| FileError::NotSource)
+    }
+
+    fn file(&self, id: FileId) -> FileResult<Bytes> {
+        Ok(Bytes::from(self.source(id)?.text().as_bytes()))
     }
 
     fn font(&self, id: usize) -> Option<Font> {
@@ -82,17 +76,14 @@ impl World for SystemWorld {
 
         slot.font
             .get_or_init(|| {
-                let data = self.file(&slot.path).ok()?;
+                let data = read(&slot.path).map(Bytes::from).ok()?;
                 Font::new(data, slot.index)
             })
             .clone()
     }
 
-    fn file(&self, path: &Path) -> FileResult<Buffer> {
-        self.slot(path)?
-            .buffer
-            .get_or_init(|| read(path).map(Buffer::from))
-            .clone()
+    fn today(&self, offset: Option<i64>) -> Option<Datetime> {
+        unimplemented!()
     }
 }
 
@@ -116,32 +107,33 @@ impl SystemWorld {
             hashes: RefCell::default(),
             paths: RefCell::default(),
             sources: FrozenVec::new(),
-            main: SourceId::detached(),
+            main: Source::detached("detached"),
         }
     }
 
     fn slot(&self, path: &Path) -> FileResult<RefMut<PathSlot>> {
         let mut hashes = self.hashes.borrow_mut();
-        let hash = match hashes.get(path).cloned() {
-            Some(hash) => hash,
-            None => {
+
+        let hash = hashes.get(path).cloned().map_or_else(
+            || {
                 let hash = PathHash::new(path);
                 if let Ok(canon) = path.canonicalize() {
-                    hashes.insert(canon.normalize(), hash.clone());
+                    hashes.insert(normalize(&canon), hash.clone());
                 }
                 hashes.insert(path.into(), hash.clone());
                 hash
-            }
-        }?;
+            },
+            |hash| hash,
+        )?;
 
         Ok(std::cell::RefMut::map(self.paths.borrow_mut(), |paths| {
             paths.entry(hash).or_default()
         }))
     }
 
-    fn insert(&self, path: &Path, text: String) -> SourceId {
-        let id = SourceId::from_u16(self.sources.len() as u16);
-        let source = Source::new(id, path, text);
+    fn insert(&self, path: &Path, text: String) -> FileId {
+        let id = FileId::new(None, VirtualPath::new(path));
+        let source = Source::new(id, text);
         self.sources.push(Box::new(source));
         id
     }
@@ -154,27 +146,44 @@ impl SystemWorld {
 
     pub fn compile(&mut self, markup: String) -> StrResult<Vec<u8>> {
         self.reset();
-        self.main = self.insert(Path::new("MARKUP.tsp"), markup);
+        self.main = self.source(self.insert(Path::new("MARKUP.tsp"), markup))?;
 
-        match typst::compile(self) {
+        let mut tracer = typst::eval::Tracer::new();
+
+        match typst::compile(self, &mut tracer) {
             // Export the PDF.
             Ok(document) => {
-                let buffer = typst::export::pdf(&document);
+                let buffer = typst::export::pdf(&document, None, None);
                 Ok(buffer)
             }
 
             // Format diagnostics.
             Err(errors) => {
                 let mut error_msg = "compile error:\n".to_string();
-                for error in *errors {
-                    let range = error.range(self);
+
+                for error in errors {
+                    let range = self
+                        .source(error.span.id().expect("the span must reference a fileid"))?
+                        .range(error.span)
+                        .expect("catches an extra check using `find`");
+
                     error_msg.push_str(&format!("{}:{} {}", range.start, range.end, error.message));
+
                     // stacktrace
                     if !error.trace.is_empty() {
                         error_msg.push_str("stacktrace:\n");
                     }
                     for point in error.trace {
-                        let range = self.source(point.span.source()).range(point.span);
+                        let range = self
+                            .source(
+                                point
+                                    .span
+                                    .id()
+                                    .ok_or(FileError::Other(Some("is detached".into())))?,
+                            )?
+                            .find(point.span)
+                            .unwrap()
+                            .range();
                         let message = point.v.to_string();
                         error_msg.push_str(&format!("  {}:{} {}", range.start, range.end, message));
                     }
@@ -314,24 +323,39 @@ impl FontSearcher {
     }
 }
 
-
 #[rustler::nif]
-fn compile<'a>(markup: String, extra_fonts: Vec<String>) -> Result<String, String> {
+fn compile(markup: String, extra_fonts: Vec<String>) -> Result<String, String> {
     let extra_fonts_paths: Vec<PathBuf> = extra_fonts.iter().map(|f| Path::new(f).into()).collect();
-   
+
     let mut world = SystemWorld::new(".".into(), extra_fonts_paths.as_slice(), &[]);
-    let result = match world.compile(markup) {
+    match world.compile(markup) {
         Ok(pdf_bytes) => {
             // the resulting string is not an utf-8 encoded string, but this is exactly what we
             // want as we are passing a binary back to elixir
-            unsafe {
-                return Ok(String::from_utf8_unchecked(pdf_bytes));
-            }
-        },
-        Err(e) => Err(e.into())
-    };
-
-    result
+            unsafe { Ok(String::from_utf8_unchecked(pdf_bytes)) }
+        }
+        Err(e) => Err(e.into()),
+    }
 }
 
 rustler::init!("Elixir.ExTypst.NIF", [compile]);
+
+/// Normalizes a path such that that it can be used as a key in a hashmap.
+///
+/// Note: code carried over from typst v0.4.0
+fn normalize(path: &Path) -> PathBuf {
+    let mut out = PathBuf::new();
+    for component in path.components() {
+        match component {
+            Component::CurDir => {}
+            Component::ParentDir => match out.components().next_back() {
+                Some(Component::Normal(_)) => {
+                    out.pop();
+                }
+                _ => out.push(component),
+            },
+            _ => out.push(component),
+        }
+    }
+    out
+}
