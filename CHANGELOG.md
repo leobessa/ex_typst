0cb8bd31de6f9b292afc4c999dbb36e3f1d0eb47
leobessa <leobessa@gmail.com>
2023-11-24 17:40:40 -0300

    Release v0.3.0-rc.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## v0.3.0-rc.1 (2023-11-24)
+
+- Add support for Typst `v0.9.0`
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExTypst.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/leobessa/ex_typst"
-  @version "0.2.3"
+  @version "0.3.0-rc.1"
 
   def project do
     [
