4c0b66eb796523c17a2d3da68b4ed2c43623766f
leobessa <leobessa@gmail.com>
2023-11-24 18:11:07 -0300

    Update precompiled package checksums

--- checksum-Elixir.ExTypst.NIF.exs
+++ checksum-Elixir.ExTypst.NIF.exs
@@ -1,9 +1,9 @@
 %{
-  "extypst_nif-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:2efc6b7f04aaa88e911c79973288e87f03ba7751a6e6b84e4247ef094409c86a",
-  "libextypst_nif-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:1dee1c3090d45b05e33fa5b049c1f7b02834adf09fc4130392569f6bb74c9393",
-  "libextypst_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9016b7ed92898d52e84c8b5f0c2d6da787b8eecbec7d4eda8eef375001d93061",
-  "libextypst_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:fe041b8434de8902c24aa281a8bb9976925c0f8dd350f1f814f5675773ad1cb4",
-  "libextypst_nif-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:8cbca7c994ee0ffef24570ef083f65f8eab60d4021f3a305aa19706fc0910745",
-  "libextypst_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:dc1bbe3f8a2a903c2958b2c7482592cfec2793c2ca576d1132dbc6e6016103fa",
-  "libextypst_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:c25d8b03950449d321112046864750c5fb2c7ff7b359742ede5f4a4dc9c239b9",
+  "extypst_nif-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:bbf7ca5b6167b798c51283fc9b52591277ca8b2afa10305d51e9a900754daa1a",
+  "libextypst_nif-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:b3c28cbc408ad7a42e194c85b1c13297fc2cb6b88f0493357d596f72a305963c",
+  "libextypst_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ba01ca952d25754b746826268695dcd47d68420dc978201bb2489619a105ea71",
+  "libextypst_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:330afc0c2eb517a7ec8d1a16170c1ce6cd0621397197f352281f0e52c5dc8a92",
+  "libextypst_nif-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:5c61cdfded71c39e5d22b41d12fb89ad76ff93e1ef74326885aed61bf65ff901",
+  "libextypst_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f0bf3d0120802c8a231ff65feea8708dee0c3ae5df75da502364502667914e3e",
+  "libextypst_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:cf06595e8613095b4e832d3605b0aa86a587fc6226f5d2bffbd1af3642646134",
 }
